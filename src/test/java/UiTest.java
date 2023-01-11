56b8ba7ecb7ae129dea2879d784a90a3dd2a81d9
Шлеткин Никита Сергеевич <nsshletkin@rt-dc.ru>
2023-01-11 21:38:54 +0300

    Убрал пробелы, определил свойства класса вверху в начале класса

--- src/test/java/UiTest.java
+++ src/test/java/UiTest.java
@@ -34,21 +34,18 @@ public class UiTest extends BaseUiTest {
     @Test
     @Tag(value = "headless")
     public void searchOtus()  {
+        MethodsHelper methodsHelper = new MethodsHelper(driver);
       logger.info("Открыли браузер в headless режиме");
-
         driver.get(duckduckgoUrl);
         logger.info("Перешли в поисковую систему");
-
         driver.findElement(By.id("search_form_input_homepage")).sendKeys("ОТУС");
         driver.findElement(By.id("search_button_homepage")).click();
         logger.info("Ввели в поисковю строку ОТУС и кликнули по кнопке поиска ");
-        MethodsHelper methodsHelper = new MethodsHelper(driver);
         methodsHelper.getElement(searchResults);
         WebElement element = driver.findElement(searchResults);
         String actual = element.getText();
         String exspected ="Онлайн‑курсы для профессионалов, дистанционное обучение современным ...";
         Assertions.assertEquals(actual,exspected);
-
         logger.info("Проверка поисковой выдачи");
 
 
@@ -56,27 +53,25 @@ public class UiTest extends BaseUiTest {
     @Test
     @Tag(value = "fullscreen")
     public void imageSearch() {
+        MethodsHelper methodsHelper = new MethodsHelper(driver);
         logger.info("Открыли браузер в fullscreen");
-
         driver.get(w3layoutsUrl);
         logger.info("Перешли на сайт");
         driver.findElement(By.xpath("//li[@data-id='id-1']")).click();
         logger.info("Кликнули по картинке");
-        MethodsHelper methodsHelper = new MethodsHelper(driver);
         Assertions.assertTrue(methodsHelper.getElement(picture).isDisplayed());
-
         logger.info("Проверили что картинка открылась в модальном окне");
-
         driver.findElement(
                 By.xpath("//img[@id='fullResImage']")).isDisplayed();
-
        }
+
     @Test
     @Tag(value = "maximize" )
     public void otusAvtorization()  {
+        Autharization autharization = new Autharization(driver);
+        MethodsHelper helper = new MethodsHelper(driver);
         driver.get(otusUrl);
         logger.info("Открыли сайт Отуса");
-        Autharization autharization = new Autharization(driver);
         autharization.auth();
         logger.info("Произвели авторизацю");
        Set<Cookie> cookiesList = driver.manage().getCookies();
@@ -84,7 +79,6 @@ public class UiTest extends BaseUiTest {
             System.out.println(getCookies);
         }
         logger.info("Вывели Cookie");
-        MethodsHelper helper = new MethodsHelper(driver);
        helper.getElement(userName);
         WebElement element = driver.findElement(userName);
         String actual = element.getText();
@@ -93,8 +87,6 @@ public class UiTest extends BaseUiTest {
         logger.info("Проверили имя указанное при регистрации на главной странице сайта");
 
     }
-
-
     }
 
 
